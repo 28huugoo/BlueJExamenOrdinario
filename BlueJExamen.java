ab2b799d569b87d300a91d42d5fe412f3209c1fe
28huugoo <150353294+28huugoo@users.noreply.github.com>
2024-06-07 09:32:17 +0200

    COMMIT FINAL DEL EXAMEN

--- BlueJExamen.java
+++ BlueJExamen.java
@@ -1,5 +1,5 @@
 public class BlueJExamen {
   public static void main(String[] args) {
-        System.out.println("Hola Mundo!!");
+        System.out.println("Este es el ejercicio 1 de Hugo");
   }
 }
